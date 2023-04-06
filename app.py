7eb81175cc75b799a385f20e330741a64fc99618
Keagan Stokoe <keagan.stokoe@gmail.com>
2023-04-06 15:46:58 +0200

    prompt updates

--- app.py
+++ app.py
@@ -61,6 +61,51 @@ def summarize_agreement(docs):
         template=refine_template,
     )
 
+    # chain required for refine approach
+    chain = load_summarize_chain(
+        ChatOpenAI(temperature=0,model_name='gpt-3.5-turbo'),
+        chain_type="refine",
+        question_prompt=PROMPT, refine_prompt=refine_prompt
+    )
+
+    # chain required for MapReduce approach
+    # chain = load_summarize_chain(
+    #     ChatOpenAI(temperature=0,model_name='gpt-3.5-turbo'),
+    #     chain_type="map_reduce",
+    # )
+
+    result = chain({"input_documents": docs}, return_only_outputs=True)
+
+    return result
+
+def identify_red_flags(docs):
+    prompt_template = """You are the world's leading expert on Non-Disclosure Agreements. You are know for reading these agreements, identifying risks that aren't covered by the agreement, and suggesting improvements that help all parties involved. You can use the context below to help you understand the agreement:
+
+    {text}
+
+    POTENTIAL THREATS AND IMPROVEMENTS:"""
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+
+    refine_template = (
+        "Your job is to identify potential threats that are not discussed in the Non-Disclosure Agreement below.\n"
+        "We have provided an existing list of threats up to a certain point: {existing_answer}\n"
+        "We have the opportunity to improve the agreement further by identifying any additional threats or red flags.\n"
+        "Your aim is to identify any potential threats to the parties involved in the agreement so that they can be mitigated.\n"
+        "The most common claims in NDA lawsuits include: breach of contract, breach of fiduciary duty, misappropriation of trade secrets, copyright infringement. \n"
+        "(only if needed) more context below.\n"
+        "------------\n"
+        "{text}\n"
+        "------------\n"
+        "Given the new context, refine the origianl threats identified.\n"
+        "If the context isn't useful, return the original list of threats."
+    )
+
+    refine_prompt = PromptTemplate(
+        input_variables=["existing_answer", "text"],
+        template=refine_template,
+    )
+
+    # chain required for refine approach
     chain = load_summarize_chain(
         ChatOpenAI(temperature=0,model_name='gpt-3.5-turbo'),
         chain_type="refine",
@@ -69,11 +114,7 @@ def summarize_agreement(docs):
     )
 
     result = chain({"input_documents": docs}, return_only_outputs=True)
-
-    return result
-
-def identify_red_flags(docs):
-    pass
+    return result['intermediate_steps'][0]
 
 def identify_suggested_improvements(docs):
     pass
@@ -81,12 +122,15 @@ def identify_suggested_improvements(docs):
 @app.route('/success')
 def success():
     docs = ingest_pdf()
-    agreement_summary = summarize_agreement(docs)
-    pretty_result = json.dumps(agreement_summary, indent=4)
-    print(pretty_result)
-    summary = agreement_summary["output_text"]
+    # agreement_summary = summarize_agreement(docs)
+    # pretty_result = json.dumps(agreement_summary, indent=4)
+    # print(pretty_result)
+    # summary = agreement_summary["output_text"]
+    summary = "summary"
 
     red_flags = identify_red_flags(docs)
+    pretty_red_flags = json.dumps(red_flags, indent=4)
+    print(pretty_red_flags)
     suggested_improvements = identify_suggested_improvements(docs)
 
     return render_template('success.html', summary=summary, red_flags=red_flags, suggested_improvements=suggested_improvements)
